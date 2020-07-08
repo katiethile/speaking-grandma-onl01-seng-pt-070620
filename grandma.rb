ea5e12cafbb96a4fcad86646d1e8e16ca513d198
Katie Le <lethikatie@gmail.com>
2020-07-08 23:38:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,7 @@ def speak_to_grandma (phrase)
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
