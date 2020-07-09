b731d2b5a3fa1c1d735c7718d8f806316af4af3a
Katie Le <lethikatie@gmail.com>
2020-07-09 00:03:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma (phrase)
   	return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
