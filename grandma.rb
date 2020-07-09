16335e2de16afb5a62e3132186c5d1ba9d95cd82
Katie Le <lethikatie@gmail.com>
2020-07-09 00:14:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.downcase
   	return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase == "I LOVE YOU GRANDMA!"
   	return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
